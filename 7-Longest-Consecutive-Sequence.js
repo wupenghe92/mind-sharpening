9f7415e5c960f4b7c12b3a6ba8004ec330adc065
WuPenghe <wupenghe92@gmail.com>
2018-04-30 18:58:06 -0400

    longestConsecutive O(n)

--- 7-Longest-Consecutive-Sequence.js
+++ 7-Longest-Consecutive-Sequence.js
@@ -5,5 +5,24 @@
  */
 
 function longestConsecutive(nums) {
+  const hash = {};
+  let longestStreak = 0;
+  for (let num of nums) {
+    hash[num] = true;
+  }
 
+  for (let numStr in hash) {
+    let currentNum = Number(numStr);
+    if (!hash[currentNum-1]) {
+      let currentStreak = 1;
+      while (hash[currentNum+1]) {
+        currentNum++;
+        currentStreak++
+      }
+      if (currentStreak > longestStreak) {
+        longestStreak = currentStreak;
+      }
+    }
+  }
+  return longestStreak
 };
