f76151889e7514de6803a34732fd5768a3c4e90f
WuPenghe <wupenghe92@gmail.com>
2018-05-02 20:18:17 -0400

    trapping water

---
+++ 8-Trapping-Rain-Water.js
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/trapping-rain-water/description/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+function trap(height) {
+  let water = 0;
+  let leftbar = height[0];
+  for (let i=0 ;i<height.length; i++) {
+
+  }
+  return water;
+};
