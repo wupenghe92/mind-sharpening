c1c082a75fc3640a1205773835ebb54cb3a2ff57
WuPenghe <wupenghe92@gmail.com>
2018-05-09 22:15:11 -0400

    topological sort

---
+++ 14-Course-Schedule.js
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/course-schedule/description/
+
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+
+};
+
+function Node(val) {
+  this.val = val;
+  this.out = [];
+}
