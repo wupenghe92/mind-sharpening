07f3668b0ece0c2fb9b848d4f14c22558c28b0a0
wupenghe92 <wupenghe@Penghes-MBP.fios-router.home>
2018-06-06 16:52:05 -0400

    1

---
+++ 24-Longest Substring with At Least K Repeating Characters.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/description/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var longestSubstring = function(s, k) {
+
+};
