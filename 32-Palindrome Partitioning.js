26775e9262e7671e0945feab3afb13d2dd925b09
wupenghe92 <wupenghe92@gmail.com>
2018-06-24 11:44:16 -0400

    backtracking recursive

--- 32-Palindrome Partitioning.js
+++ 32-Palindrome Partitioning.js
@@ -8,5 +8,22 @@ Return all possible palindrome partitioning of s.
  * @return {string[][]}
  */
 var partition = function(s) {
-  
+  if (s.length === 0) return [];
+  // if (s.length === 1) return [[s]];
+  let ans = [];
+  for (let i=0; i<s.length; i++) {
+    let subs = s.substring(0,i+1);
+    if (isPalindrome(subs)) {
+      const restPartition = partition(s.substring(i+1));
+      for (let j=0; j<resPartition.length; j++) {
+        resPartition[j].unshift(subs);
+        ans.push(resPartition[j]);
+      }
+    }
+  }
+  return ans;
 };
+
+function isPalindrome(s) {
+  return s === s.split('').reverse().join('');
+}
