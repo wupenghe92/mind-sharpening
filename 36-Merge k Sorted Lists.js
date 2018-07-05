08d4ca4007b45f13ec19f8fa4e000b805f06c16c
wupenghe92 <wupenghe92@gmail.com>
2018-07-05 12:24:32 -0400

    cut cake

--- 36-Merge k Sorted Lists.js
+++ 36-Merge k Sorted Lists.js
@@ -20,9 +20,42 @@
 Output: 1->1->2->3->4->4->5->6
 
  */
-var mergeKLists = function(lists) {
+const mergeKLists = function(lists) {
+  if (lists.length === 0) return [];
   const heads = [];
   for (let i=0; i<lists.length; i++) {
-    
+    if (lists[i])  heads.push(lists[i]);
+  }
+  if (heads.length === 0) return [];
+  let idx = findMin();
+  let result = heads[idx];
+  let current = result;
+  updateHeads(idx);
+  while(heads.length > 0) {
+    idx = findMin();
+    current.next = heads[idx];
+    updateHeads(idx)
+    current = current.next;
+  }
+  return result;
+
+  function findMin() {
+    let idx = 0;
+    let min = heads[0].val;
+    for (let i=1; i<heads.length; i++) {
+      if (heads[i].val < min) {
+        idx = i;
+        min = heads[i].val;
+      }
+    }
+    return idx;
+  }
+
+  function updateHeads(idx) {
+    if (heads[idx].next !== null) {
+      heads[idx] = heads[idx].next;
+    } else {
+      heads.splice(idx, 1);
+    }
   }
 };
---
+++ vimbly/undefined.js
@@ -0,0 +1,36 @@
+
+
+
+function solution(X, Y, K, A, B) {
+  const xarr = findLength(X, A);
+  const yarr = findLength(Y, B);
+  const pieces = [];
+  for (let i=0; i<xarr.length; i++) {
+    for (let j=0; j<yarr.length; j++) {
+      pieces.push(xarr[i]*yarr[j]);
+      // min-heap / prority queue
+    }
+  }
+  pieces.sort( (a,b) => b-a); // O(n^2 * log(n^2))  vs O(n^2 * log(k))
+  return pieces[K-1];
+}
+
+function findLength(L, positions) {
+  const larr = [positions[0]];
+  for (let i=1; i<positions.length; i++) {
+    larr.push(positions[i] - positions[i-1]);
+  }
+  larr.push(L-positions[positions.length-1]);
+  return larr;
+}
+
+// let X = 6,   Y = 7,   K = 3;
+// const A = [1, 3]
+// const B = [1, 5]
+
+let X = 35 ,  Y = 38 ,  K = 15;
+const A = [1, 3, 6, 11, 24, 26, 32]
+const B = [1, 5, 8, 13, 16, 22, 26]
+
+
+console.log(solution(X, Y, K, A, B));
